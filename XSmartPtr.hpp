b138c59da40423e0bd1e408f94803276ed09a381
xulifei23 <54622518@qq.com>
2015-11-06 17:30:41 +0800

    Update XSmartPtr.hpp

--- XSmartPtr.hpp
+++ XSmartPtr.hpp
@@ -1,8 +1,60 @@
 #ifndef XSMARTPTR_HPP
 #define XSMARTPTR_HPP
 
+#ifndef DISALLOW_COPY_AND_ASSIGN
+#define DISALLOW_COPY_AND_ASSIGN(ClassName) \
+	ClassName(const ClassName&); \
+	ClassName& operator =(const ClassName&)
+#endif // DISALLOW_COPY_AND_ASSIGN
+
+#ifndef FREE_POINTER
+#define FREE_POINTER(p) \
+	if (p) { delete p; p = NULL; }
+#endif // FREE_POINTER
+
+#ifndef FREE_ARRAY
+#define FREE_ARRAY(pArray) \
+	if (pArray) { delete [] pArray; pArray = NULL; }
+#endif // FREE_ARRAY
+	
+
 namespace XSMARTPTR
 {
+	// XScopePtr
+	template<class T>
+	class XScopePtr
+	{
+		typedef XScopePtr<T> ThisType;
+		
+	public:
+	
+		// ctor
+		explicit XScopePtr(T* pObj = NULL)
+			:m_pObj(pObj)
+		{
+			
+		}
+		// dector
+		~XScopePtr()
+		{
+			FREE_POINTER(m_pObj);
+		}
+		// reset
+		void Reset(T* pOtherObj)
+		{
+			if (pOtherObj == NULL)
+			{
+				throw exception("null ptr");
+			}
+			
+			
+		}
+		
+	private:
+	
+		DISALLOW_COPY_AND_ASSIGN;
+		T* m_pObj;
+	};
 	
 }
 
