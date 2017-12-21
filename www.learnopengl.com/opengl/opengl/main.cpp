07a73fbd8a34b7a79876a13a3da858ad30830fe7
xulifei23 <54622518@qq.com>
2017-12-21 17:15:24 +0800

    add texture

--- www.learnopengl.com/opengl/opengl/main.cpp
+++ www.learnopengl.com/opengl/opengl/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "global.h"
 #include "shader.h"
+#include "stb_image.h"
 
 using namespace std;
 
@@ -33,9 +34,16 @@ int main()
 
 	float vertices[] = 
 	{
-		0.0f, 0.5f, 0.0f, 
-		0.5f, -0.5f, 0.0f, 
-		-0.5f, -0.5f, 0.0f
+		0.5f, 0.5f, 0.0f,      1.0f, 1.0f,     1.0f, 0.0f, 0.0f, 
+		0.5f, -0.5f, 0.0f,     1.0f, 0.0f,     0.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f,    0.0f, 0.0f,     0.0f, 0.0f, 1.0f, 
+		-0.5f, 0.5f, 0.0f,     0.0f, 1.0f,     0.0f, 1.0f, 0.0f
+	};
+
+	unsigned int indices[] = 
+	{
+		0, 1, 2, 
+		2, 3, 0
 	};
 
 	float texCoords[] = 
@@ -45,11 +53,21 @@ int main()
 		0.5f, 1.0f
 	};
 
+	int width = 0, height = 0, nrChannels = 0;
+	unsigned char* data = stbi_load("../textures/wall.jpg", &width, &height, &nrChannels, 0);
+	unsigned int texture = 0;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	stbi_image_free(data);
+
+
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 	unsigned int VAO = 0;
 	glGenVertexArrays(1, &VAO);
@@ -58,8 +76,16 @@ int main()
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (void*)0);
+	unsigned int EBO = 0;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (void*)(3 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (void*)(5 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(2);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
@@ -74,7 +100,7 @@ int main()
 
 		shader.use();
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -82,6 +108,7 @@ int main()
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glfwTerminate();
 	return 0;
 }
--- www.learnopengl.com/opengl/shaders/shader.vs
+++ www.learnopengl.com/opengl/shaders/shader.vs
@@ -1,6 +1,14 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTexCoord;
+layout (location = 2) in vec3 aColor;
+
+out vec2 TexCoord;
+out vec3 vColor;
+
 void main()
 {
 	gl_Position = vec4(aPos.x, aPos.y, aPos.y, 1.0f);
+	TexCoord = aTexCoord;
+	vColor = aColor;
 }
